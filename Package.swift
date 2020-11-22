b12ec58a7537c624f5781a77731bd0871a589f8b
Woody <woodymelling@gmail.com>
2020-11-21 21:20:49 -0700

    Docker things

--- Package.swift
+++ Package.swift
@@ -12,9 +12,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
 
         // Core
-        .package(path: "/Users/woody/source/Festivl/FestivlCore")
+        .package(url: "https://github.com/Festivl/festivl-core", from: "0.0.0")
     ],
     targets: [
         .target(
@@ -22,9 +23,10 @@ let package = Package(
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Leaf", package: "leaf"),
-                .product(name: "FestivlCore", package: "FestivlCore")
+                .product(name: "FestivlCore", package: "festivl-core")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,7 @@
 import Fluent
 import FluentSQLiteDriver
+import FluentPostgresDriver
+import Fluent
 import Vapor
 import Leaf
 
@@ -11,9 +13,14 @@ public func configure(_ app: Application) throws {
     app.views.use(.leaf)
     app.leaf.cache.isEnabled = app.environment.isRelease
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    if true {
+        setUpPostgres(app)
+    } else {
+        setUpSQLite(app)
+    }
 
     addMigrations(app)
+    setUpCors(app)
 
     // register routes
     try routes(app)
@@ -30,4 +37,32 @@ private func addMigrations(_ app: Application) {
     app.migrations.add(UserToken.Migration())
     app.migrations.add(UserRole.Migration())
     app.migrations.add(FestivalUserRole.Migration())
+}
+
+private func setUpCors(_ app: Application) {
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    let error = ErrorMiddleware.default(environment: app.environment)
+    // Clear any existing middleware.
+    app.middleware = .init()
+    app.middleware.use(cors)
+    app.middleware.use(error)
+}
+
+private func setUpSQLite(_ app: Application) {
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+}
+
+private func setUpPostgres(_ app: Application) {
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql)
 }
